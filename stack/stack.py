c6027366791aaae9a9f8f2cafaae9bd77b3623c2
fshahinfar1 <fsh1.dev@gmail.com>
2017-11-19 13:22:20 +0330

    create: add stack folder and implement a stack class

---
+++ stack/stack.py
@@ -0,0 +1,49 @@
+'''
+Farbod Shahinfar
+95521216
+Data Structure Course
+Fall 96-97
+HW1
+Stack
+'''
+
+class stack(object):
+    def __init__(self):
+        '''
+        create an empty stack
+        '''
+        self.__container = []  # type: list
+        self.__top = -1  # index of the top element in the list
+
+    def top(self):
+        if self.__top == -1:
+            raise Exception('Top operation on an empty list')
+        return self.__container[self.__top]
+
+    def pop(self):
+        del self.__container[self.__top]
+        self.__top -= 1
+
+    def push(self, value):
+        self.__container[self.__top+1:] = [value]
+        self.__top += 1
+
+    def size(self):
+        return self.__top + 1
+
+    def is_empty(self):
+        if self.__top == -1:
+            return True
+        return False
+
+    def __len__(self):
+        return self.size()
+
+    def __repr__(self):
+        representation = []
+        for i in self.__container:
+            representation.append(str(i))
+        return "stack: "+", ".join(representation)
+
+    def __str__(self):
+        return self.__repr__()
